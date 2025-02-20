da72d1c922dfac9bc687fdb8401aed79943b2a0a
siyeonpk <siyeonpk@gmail.com>
2025-02-20 14:51:17 +0900

    first commit

---
+++ src/app/add/page.tsx
@@ -0,0 +1,12 @@
+
+
+const page = () => (
+  <div>
+
+     <main>
+		    <h1>New Bookmark Form</h1>
+		 </main>   
+  </div>
+);
+
+export default page;
---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+
+
+export default function Home() {
+  return (
+    <div>
+    
+      <main>
+        <h1>Welcome to Bookmarker</h1>
+      </main>
+    </div>
+  );
+}
